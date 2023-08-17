5c53d2a7908a0ba5dc3e691756603c0c265665b9
biabuluo <1802362935@qq.com>
2023-08-17 15:53:04 +0800

    刷题日记：8/17 随机一题

--- src/random/t848.java
+++ src/random/t848.java
@@ -7,8 +7,18 @@ package random;
  * @description: 字母移位
  */
 public class t848 {
+    //思路：前缀和
     public String shiftingLetters(String s, int[] shifts) {
-        //todo
-        return "";
+        int len = shifts.length;
+        shifts[len-1] %= 26;
+        for(int i=len-2;i>=0;i--){
+            shifts[i] = (shifts[i]+shifts[i+1])%26;
+        }
+        //遍历数组
+        char []str = s.toCharArray();
+        for(int i=0;i<len;i++){
+            str[i] = (char)('a'+(str[i]-'a'+shifts[i])%26);
+        }
+        return String.valueOf(str);
     }
 }
